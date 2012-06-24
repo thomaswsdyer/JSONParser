1082b113d5832340ebda8ac3b740ae95ce070fee
tomdyer <tom.dyer@gmail.com>
2012-06-24 11:22:16 -0600

    Added more attributes to be parsed out. Added puts to show attributes. Updated README

--- parser.rb
+++ parser.rb
@@ -1,3 +1,32 @@
+#
+# File:        parser.rb
+# Descritpion: Ruby script that reads a pre-canned json file to extract product release information and RPM URLs.
+#              Mostly used to play around with how to parse josn and use the resulting attributes.
+#
+# Author:      Tom Dyer
+# GitHub:      http://github.com/tomdyer/JSONparser
+
 require 'json'
 
-packageName = JSON.parse(File.read('package.json'))
+releaseInfo = JSON.parse(File.read('releaseInfo.json'))
+
+#
+# Grab product attributes
+#
+product = releaseInfo['release']['product']
+version = releaseInfo['release']['version']
+revision = releaseInfo['release']['revision']
+
+distroRPM = releaseInfo['packages']['distribution']
+standaloneRPM = releaseInfo['packages']['standalone']
+
+
+#
+# Download RPM packages
+#
+#
+p "Downloading install packages for #{product} #{version}-#{revision}"
+# %x[wget #{distroRPM}]     # Lightweight distribution
+# %x[wget #{standaloneRPM}] # Not so lightweight standalone install (very large, has product binaries)
+
+p "Is this was a chef cookbook, we could install these packages now."
